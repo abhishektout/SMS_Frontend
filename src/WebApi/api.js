56c5a510d32d5cd7f50c0f6531ec0b28c3a861dd
shivani <patelshivani3008@gmail.com>
2023-11-02 15:28:57 +0530

    Chnage base url

--- src/WebApi/api.js
+++ src/WebApi/api.js
@@ -2,7 +2,7 @@ export default{
    STUDENT_REGISTRATION:'/student/registration',
    FETCH_CLASS_FEE:'/student/fetchFee',
    VERIFY_TRANSACTION_ID:'/accountInfo/verifyTransactioId',
-   URL_S:'http://localhost:3036',
+   URL_S:'https://sms-backend-6.onrender.com',
    ADMIN_SIGNIN:'/admin/signIn',
    STUDENT_ATTENDANCE:'/student/studentAttendance',
    CHECKALREADYTRANSACTIONID :'/student/checkAlreadyTransactionId',
